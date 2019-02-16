f2b16cab765ff008fe0ca397e85faf2cf647779a
Albert George <albertgeorge@Alberts-MacBook-Pro.local>
2019-02-16 01:19:30 -0500

    update add two numbers

--- AddTwoNumbers.c
+++ AddTwoNumbers.c
@@ -7,23 +7,80 @@
  */
 int count(struct ListNode* listPtr);
 struct ListNode* appendZeros(struct ListNode* shortList, int amount);
+struct ListNode* pushNode(int value, struct ListNode* listPtr);
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    
+    int addend1, addend2, sum;
+    struct ListNode* l3 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    int carry = 0;
     int sameSize = count(l1) - count(l2);
+    //lists should be in the same size
     if (sameSize){
         if(sameSize>0){
-            printf("address of l2: %d \n", l2);
+            //printf("address of l2: %d \n", l2);
             l2 = appendZeros(l2, sameSize);
-            printf("size of l2: %d\n", count(l2));
+            //printf("size of l2: %d\n", count(l2));
         }
         if(sameSize<0){
-            printf("address of l1: %d \n", l1);
+            //printf("address of l1: %d \n", l1);
             l1 = appendZeros(l1, sameSize*(-1));
-            printf("size of l1: %d\n", count(l1));
+            //printf("size of l1: %d\n", count(l1));
         }
     }
+    
+    while(1){
+        sum = 0;
+        addend1 = l1->val; 
+        addend2 = l2->val;
+        sum = addend1+addend2+carry;
+        printf("l1 is %d; l2 is %d; l3 is %d\n", addend1, addend2, l3->val);
+        if(sum > 10){
+            sum = sum % 10;
+            carry = 1;
+        }
+        else{
+            carry = 0;
+        }
+        //push sum into new list
+        l3 = pushNode(sum, l3);
+        //check if EOL
+        if(!(l1->next)){
+            if (carry == 1){
+                l3 = pushNode(1, l3);
+                return l3;
+            }   
+            return l3;
+        }         
+        l1 = l1->next;
+        l2 = l2->next;    
+    }
+    //add nodes carry over check
+    //push into new list
     return 0; 
+    
+}
+struct ListNode* pushNode(int value, struct ListNode* listPtr){
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* tempPtr = (struct ListNode*)malloc(sizeof(struct ListNode));
+    tempPtr = listPtr;
+    
+    newNode->val = value;
+    newNode->next = NULL;
+    
+    if(listPtr == NULL){
+        listPtr = newNode;
+        return listPtr;
+    }
+    
+    while(tempPtr->next != NULL){
+        tempPtr = tempPtr->next;     
+    }
+    tempPtr->next = newNode;
+    newNode = NULL;
+    tempPtr = NULL;
+    free(newNode);
+    free(tempPtr);
+    return listPtr;
     
 }
 struct ListNode* appendZeros(struct ListNode* shortList, int amount){
@@ -38,7 +95,7 @@ struct ListNode* appendZeros(struct ListNode* shortList, int amount){
         }
     }
     while(amount){
-        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));;
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
         newNode->val = 0;
         newNode->next = NULL;
         tempNode->next =newNode;
@@ -54,40 +111,7 @@ int count(struct ListNode* listPtr) {
  int counter = 0; 
   while(listPtr != NULL){   
    counter++;
-   //printf("In COUNT: %d \n", listPtr->val);
    listPtr = listPtr->next;   
   }
  return counter;
 } 
-// struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-//     struct ListNode* l3 = (struct ListNode*)malloc(sizeof(struct ListNode)); 
-    
-//     int addend1, addend2, sum; 
-//     int carry = 0;
-    
-//     //printf("Testing COUNT: %d, %d", count(l1), count(l2));
-   
-//   while(count(l1)||count(l2)){ 
-    
-//         addend1 = pop(&l1);
-//         addend2 = pop(&l2);
-//         sum = addend1 + addend2 + carry;
-      
-//     //printf("Testing POP %d %d %d \n", addend1, addend2, sum); //Test POP
-
-//        if (sum  >= 10){
-//             sum = sum % 10;
-//             carry = 1;
-//         }
-//         else{
-//             carry = 0;
-//         }
-//         //printf("Pushing %d:\n", sum);
-//         push(&l3, sum);
-//        //printf("Testing PUSH: %d\n", pop(&l3)); 
-      
-//    }
-//     //printf("testing everything: %d %d %d\n", pop(&l3), pop(&l3), pop(&l3));
-    
-//     return &l3;
-// }
