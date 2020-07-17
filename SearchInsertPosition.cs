b8d8c7b176936c72daeb1b110e5a3eee5b10ab65
Albert <atg5215@gmail.com>
2020-07-17 11:40:45 -0400

    Switched from binary search to linear search

--- SearchInsertPosition.cs
+++ SearchInsertPosition.cs
@@ -1,34 +1,31 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        bool roundNotFound = true;
         int arrayLen = nums.Length;
         if(arrayLen == 0){
             return 0;
         }
         int lowerBound = 0;
         int upperBound = arrayLen-1;
-        int middle; 
+        int i = 0; 
         
+       
         if(target > nums[upperBound]){
             return upperBound+1;
         }
         else if(target <= nums[lowerBound]){
             return lowerBound;
-        }  
-        while(true){
-            middle = lowerBound+upperBound/2;
-            if(nums[middle]>target){
-                upperBound = middle;
-            }
-            else if(nums[middle]<target){
-                lowerBound = middle;
-            }
-            if(nums[middle]==target)
-                return middle;
-            if(upperBound-lowerBound == 1){
-                return upperBound;    
-            }              
         }
+        
+        while(i<arrayLen){
+            if(nums[i]==target){
+                return i;
+            }
+            else if((nums[i]<target)&&(target<nums[i+1])){
+                return i+1;
+            }
+            i++;
+        }
+        
         return 0;
     }
 }
