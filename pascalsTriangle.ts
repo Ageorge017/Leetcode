6d57c598bb6b3c508fe1877721a896bac08ad313
Albert <atg5215@gmail.com>
2021-06-21 20:56:21 -0400

    pascals triangle complete

---
+++ pascalsTriangle.ts
@@ -0,0 +1,15 @@
+function generate(numRows: number): number[][] {
+    var pascalsTriangle:number[][] = [[1]];
+    var i:number
+    for(i = 1; i<=numRows-1; i++){
+        const currentRow:number[] = pascalsTriangle[i-1];
+        var j:number;
+        var newRow:number[] = [1];
+        for(j=0; j<currentRow.length-1; j++){
+            newRow.push(currentRow[j]+currentRow[j+1])
+        }
+        newRow.push(1);
+        pascalsTriangle.push(newRow);
+    }
+    return pascalsTriangle;
+};
