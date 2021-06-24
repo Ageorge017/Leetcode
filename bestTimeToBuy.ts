718eecb720cbe8467a12a6bab9a3fbb02c8b284a
Albert <atg5215@gmail.com>
2021-06-24 00:06:49 -0400

    best time to buy complete

---
+++ bestTimeToBuy.ts
@@ -0,0 +1,13 @@
+function maxProfit(prices: number[]): number {
+    var minPrice:number = 10001;
+    var maxProfit: number = 0;
+    var i:number;
+    
+    for(i = 0; i<prices.length; i++){
+        if (prices[i] < minPrice)
+            minPrice = prices[i];
+        else if (prices[i] - minPrice > maxProfit)
+            maxProfit = prices[i] - minPrice;
+    }
+    return maxProfit
+};
